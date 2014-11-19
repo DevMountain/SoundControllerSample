49f8d87f3550996a08e43d5f7588855bc5df593e
Caleb Hicks <calebhicks@gmail.com>
2014-11-18 18:32:15 -0700

    Initial Commit

---
+++ SoundController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundController
+//
+//  Created by Caleb Hicks on 11/18/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
